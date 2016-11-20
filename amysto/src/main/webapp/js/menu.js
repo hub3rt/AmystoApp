049ac93273115550694070b3b4d4b7b724ffe9f7
nruiz64 <Rocklee65&>
2016-11-20 22:44:42 +0100

    Add webapp

---
+++ amysto/src/main/webapp/js/menu.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+
+  $('.icon-menu').click(function() {
+    var state = $('.menu').data('state');
+    if(state === 'close'){
+      $('.menu').animate({     
+        left: "0px"    
+      }, 200);
+
+      $('body').animate({ 
+        left: "200px"
+      }, 200);
+      $('.menu').data('state', 'open');
+    }else if(state === 'open'){
+      $('.menu').animate({     
+        left: "0px"    
+      }, 200);
+
+      $('body').animate({ 
+        left: "-200px"
+      }, 200);
+      $('.menu').data('state', 'close');
+    }
+  });
+
+});
+
