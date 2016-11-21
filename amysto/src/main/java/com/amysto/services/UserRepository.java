923f1bd3b096bed8b3029fcf2b75420d907d3643
nruiz <nruiz@soprasteria.com>
2016-11-21 17:07:23 +0100

    Refacto code

---
+++ amysto/src/main/java/com/amysto/services/UserRepository.java
@@ -0,0 +1,15 @@
+package com.amysto.services;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
+public interface UserRepository extends MongoRepository<User, String> {
+
+	List<User> findAll();
+	List<User> findByLastName(@Param("name") String name);
+
+}
