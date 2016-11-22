f2d86a24664fae616c5d21d83559d1a040a94aa7
nruiz <nruiz@soprasteria.com>
2016-11-22 17:04:21 +0100

    conf servlet modifiée

--- amysto/src/main/java/com/amysto/web/MainController.java
+++ amysto/src/main/java/com/amysto/web/MainController.java
@@ -1,14 +1,13 @@
 package com.amysto.web;
 
-import javax.servlet.http.HttpServlet;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class MainController extends HttpServlet {
+@RequestMapping("/")
+public class MainController {
 
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
