44895f153164062810f78ee19c66958e01525819
nruiz <nruiz@soprasteria.com>
2016-11-22 18:13:54 +0100

    Test Spring MVC 3 from SpringIO

--- amysto/src/main/java/com/amysto/config/WebInitializer.java
+++ amysto/src/main/java/com/amysto/config/WebInitializer.java
@@ -11,29 +11,29 @@ import org.springframework.web.servlet.DispatcherServlet;
 
 import com.amysto.Main;
 
-public class WebInitializer implements WebApplicationInitializer {
-
-	@Override
-    public void onStartup(ServletContext container) {
-      // Create the 'root' Spring application context
-      AnnotationConfigWebApplicationContext rootContext =
-        new AnnotationConfigWebApplicationContext();
-      rootContext.register(Main.class);
-
-      // Manage the lifecycle of the root application context
-      container.addListener(new ContextLoaderListener(rootContext));
-
-      // Create the dispatcher servlet's Spring application context
-      AnnotationConfigWebApplicationContext dispatcherContext =
-        new AnnotationConfigWebApplicationContext();
-      dispatcherContext.register(SpringWebConfig.class);
-
-      // Register and map the dispatcher servlet
-      ServletRegistration.Dynamic dispatcher =
-        container.addServlet("dispatcher", new DispatcherServlet(dispatcherContext));
-      dispatcher.setLoadOnStartup(1);
-      dispatcher.addMapping("/");
-    }
-
-}
+//public class WebInitializer implements WebApplicationInitializer {
+//
+//	@Override
+//    public void onStartup(ServletContext container) {
+//      // Create the 'root' Spring application context
+//      AnnotationConfigWebApplicationContext rootContext =
+//        new AnnotationConfigWebApplicationContext();
+//      rootContext.register(Main.class);
+//
+//      // Manage the lifecycle of the root application context
+//      container.addListener(new ContextLoaderListener(rootContext));
+//
+//      // Create the dispatcher servlet's Spring application context
+//      AnnotationConfigWebApplicationContext dispatcherContext =
+//        new AnnotationConfigWebApplicationContext();
+//      dispatcherContext.register(SpringWebConfig.class);
+//
+//      // Register and map the dispatcher servlet
+//      ServletRegistration.Dynamic dispatcher =
+//        container.addServlet("dispatcher", new DispatcherServlet(dispatcherContext));
+//      dispatcher.setLoadOnStartup(1);
+//      dispatcher.addMapping("/");
+//    }
+//
+//}
 
--- amysto/src/main/java/com/amysto/web/MainController.java
+++ amysto/src/main/java/com/amysto/web/MainController.java
@@ -6,11 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
 public class MainController {
-
-
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/greeting")
 	public String index(ModelMap model) {
 
 		model.addAttribute("message", "Spring 3 MVC Hello World");
