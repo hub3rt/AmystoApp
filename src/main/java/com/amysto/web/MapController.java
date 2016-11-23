7d0e4c8fcd27e9b5ae690fa01c9404f5420d72f5
nruiz64 <Rocklee65&>
2016-11-23 15:36:51 +0100

    Comment controller.

--- src/main/java/com/amysto/web/MapController.java
+++ src/main/java/com/amysto/web/MapController.java
@@ -3,13 +3,14 @@ package com.amysto.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-@RequestMapping("/map")
-public class MapController {
-
-    @RequestMapping(method = RequestMethod.GET)
-    public String index() {
-    	return "map";
-    }
-}
+//@Controller
+//@RequestMapping("/map")
+//public class MapController {
+//	
+////    @RequestMapping(method = RequestMethod.GET)
+////    public String index() {
+////    	return "map/map.html";
+////    }
+//}
