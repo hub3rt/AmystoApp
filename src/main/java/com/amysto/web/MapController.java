523c26bfd56f4fe74ab8d8b0783318e90bea4f34
nruiz64 <Rocklee65&>
2016-11-23 14:26:40 +0100

    just one error left.

--- src/main/java/com/amysto/web/MapController.java
+++ src/main/java/com/amysto/web/MapController.java
@@ -1,10 +1,10 @@
 package com.amysto.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/map")
 public class MapController {
 
