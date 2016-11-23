2c4e29c1af34fa0ad09761a0204c4219ec141352
nruiz64 <Rocklee65&>
2016-11-23 13:02:24 +0100

    Add Map controller

---
+++ src/main/java/com/amysto/web/MapController.java
@@ -0,0 +1,15 @@
+package com.amysto.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/map")
+public class MapController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String index() {
+        return "map";
+    }
+}
