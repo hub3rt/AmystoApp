46b41b547e04171def8daefa5407d2f7f3333d83
nruiz64 <Rocklee65&>
2016-11-23 12:08:55 +0100

    Change archi front

--- src/main/java/com/amysto/web/MainController.java
+++ src/main/java/com/amysto/web/MainController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/main")
 public class MainController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String index() {
-        return "index";
+        return "main";
     }
 }
