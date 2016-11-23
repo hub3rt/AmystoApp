1fb281eee81103ac352336f00eaacf7715ea30f6
nruiz64 <Rocklee65&>
2016-11-23 13:20:21 +0100

    clean

--- src/main/java/com/amysto/web/MapController.java
+++ src/main/java/com/amysto/web/MapController.java
@@ -1,8 +1,10 @@
 package com.amysto.web;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/map")
@@ -10,6 +12,6 @@ public class MapController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String index() {
-        return "map";
+    	return "map";
     }
 }
